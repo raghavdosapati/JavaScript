b48b23da772df67ae68175a758cb42681770bf24
Raghavendra_Dosapati <136189054+raghavdosapati@users.noreply.github.com>
2024-04-03 21:04:03 +0530

    Add files or upload

---
+++ DatatypesAndObjects.js
@@ -0,0 +1,19 @@
+//nn bb ss u - null number bigInt boolean symbol string undefined
+let a=null;
+let b=233;
+let c=BigInt("454")+BigInt("34");
+let d=false;
+let e=Symbol("Hey fellass..!");
+let f="its string";
+let g=undefined;
+console.log(a,b,c,d,e,f,g)
+console.log(typeof e)
+console.log(typeof d)
+
+//objects in javascript
+const obj={
+  "raghav":33,
+  "ranesh":false,
+  "mathura":"place"
+}
+console.log(obj["raghav"])
