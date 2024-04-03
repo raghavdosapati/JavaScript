1b46416838766bcba2dd2f7e03597cf8e9b09825
Raghavendra_Dosapati <136189054+raghavdosapati@users.noreply.github.com>
2024-04-03 22:57:30 +0530

    Add files or upload

---
+++ OperatordAndExpressions.js
@@ -0,0 +1,29 @@
+console.log("operaors in js")
+let a=45;
+let b=4;
+  //  Arithmeitc operators
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+console.log("a**b=",a**b)
+console.log("a%b=",a%b)
+console.log("a++=",a++)
+console.log("a=",a)            // increment and decrement operators
+console.log("++a=",++a)
+console.log("a--=",a--)
+console.log("a=",a)
+console.log("--a=",--a)
+let comp1=6;
+let comp2="6";
+    //Comparision operators
+console.log("comp1==comp2 is",comp1==comp2)
+console.log("comp1!=comp2 is",comp1!=comp2)
+console.log("comp1===comp2 is",comp1===comp2)    //=== checks also the type if both are equal in value and type its true or false
+console.log("comp1!==comp2 is",comp1!==comp2)
+let x=5;
+let y=6;
+    //Bitwise operators
+console.log(x<y && x==5)
+console.log(x>y || x==5)      //throws result as boolean values
+console.log(!false)
