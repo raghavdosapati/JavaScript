c45751b73e9710211671fe0778277651b041a3d8
Raghavendra_Dosapati <136189054+raghavdosapati@users.noreply.github.com>
2024-05-04 09:06:52 +0530

    Add files or upload

--- functions.js
+++ functions.js
@@ -14,3 +14,16 @@ console.log(marks(a,b))
 // we can declare variables to assign functions
 var add=new Function("num1","num2","return num1+num2");  
 console.log(add(2,5));
+
+
+// --ARROW FUNCTIONS
+/*An arrow function expression is a compact alternative to a traditional function expression, with some semantic differences and deliberate limitations in usage:*/
+const materials = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium'];
+console.log(materials.map((material) => material.length));
+// Expected output: Array [8, 6, 7, 9]
+
+   //OR
+const sheet =(a, b, c, d)=>{
+  return (a+ b+ c+ d)/(a+b)
+}
+console.log(sheet(2, 3, 4, 5))
