d34f97aa672db729d38d5248b28091858e685471
Raghavendra_Dosapati <136189054+raghavdosapati@users.noreply.github.com>
2024-06-06 16:52:36 +0530

    Update addEventListener.js

--- addEventListener.js
+++ addEventListener.js
@@ -26,43 +26,43 @@ if (a == "2") {
 --------------------------------------------------------
 // HTML CODE
 
-// <!DOCTYPE html>
-// <html>
+<!DOCTYPE html>
+<html>
 
-// <head>
-//   <meta charset="utf-8">
-//   <meta name="viewport" content="width=device-width">
-//   <title>JavaScript</title>
-//   <link href="style.css" rel="stylesheet" type="text/css" />
-// </head>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JavaScript</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
 
-// <!-- <body>
-//   <div  id="first" class="red">
-//     <span><b>this is span tag</b></span>
-//   </div>  
-//   <script src="demo.js"></script>
+<!-- <body>
+  <div  id="first" class="red">
+    <span><b>this is span tag</b></span>
+  </div>  
+  <script src="demo.js"></script>
 
-//   <!--
-//   This script places a badge on your repl's full-browser view back to your repl's cover
-//   page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-//   teal, blue, blurple, magenta, pink!
-//   -->
-//   <!-- <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> -->
-// <!-- </body> -->
+  <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+  <!-- <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> -->
+<!-- </body> -->
 
 
-// <body>
-//   <div>
-//     <button id="btn">Click Me</button>
-//   </div>
-//   <script src="demo.js"></script>
+<body>
+  <div>
+    <button id="btn">Click Me</button>
+  </div>
+  <script src="demo.js"></script>
 
-//   <!--
-//   This script places a badge on your repl's full-browser view back to your repl's cover
-//   page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-//   teal, blue, blurple, magenta, pink!
-//   -->
-//   <!-- <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> -->
-// </body>
+  <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+  <!-- <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> -->
+</body>
 
-// </html>
+</html>
